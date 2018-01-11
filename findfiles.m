909483c38c9a899f5657f11c024ba6be1f2cf1b9
Adrienne <eddi@Adriennes-MacBook-Pro-2.local>
2018-01-10 22:49:43 -0800

    Made make_master_file_struct automatic instead of manual.

---
+++ findfiles.m
@@ -0,0 +1,32 @@
+function [ files ] = findfiles( folder, pattern, ret_direc )
+%FINDFILES Find all files in a folder, matching the pattern.
+
+    files = [];
+    matches = dir(fullfile(folder, pattern));
+    % Don't return folders
+    if (ret_direc == 0)
+        matches = matches([matches.isdir] == 0);
+    end
+
+    % Return full file names
+    for i = 1: length(matches)
+        match = matches(i);
+        match.name = fullfile(folder, match.name);
+        files = [files ; match];
+    end
+    
+    folders = dir(folder);
+    folders = folders([folders.isdir] == 1);
+
+    for i = 1: length(folders)
+        child = folders(i);
+        if ~strcmp(child.name, '.') && ~strcmp(child.name, '..')
+            deeperFiles = findfiles( fullfile(folder, child.name), pattern, ret_direc );
+            files = [files ; deeperFiles];
+        else
+            %disp(['Skipping ' child.name]);
+        end
+    end
+
+end
+
