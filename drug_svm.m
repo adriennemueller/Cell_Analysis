3a02ecbc1721129fc85e8d39d57b0eee94d5ae5b
adriennemueller <adrienne.l.mueller@gmail.com>
2018-05-30 17:07:37 -0700

    Finished making drug_svm work! Now bootstraps

--- drug_svm.m
+++ drug_svm.m
@@ -1,6 +1,22 @@
+% drug_svm takes:
+% master_file_struct
+% drug_selection (either SCH23390 or SKF81297, currently)
+% current: for that drug selection (usually either 20 or 50)
+% straps: the number of crossvalidation iterations to run
 
-function rslt = drug_svm( mfs, drug_selection, current_selection )
+% It will create one large matrix of trials from cells which were exposed
+% to the chosen drug at the chosen current and give each trial a label of
+% either 'Control' or 'Drug'. It will then train 'straps'-many SVM
+% classifiers on that data, with the matrix being broken into 'straps'-many
+% sets of training and test matrices.
 
+% The output struct includes the models, the labels, the scores and the percent of
+% the labels that were correct for each model. The struct will also
+% include, for each model, the percentage of the training matrix trials
+% that were control trials.
+function rslt = drug_svm( mfs, drug_selection, current_selection, straps )
+    
+    if nargin < 4, straps = 100; end
 
     % Get giant matrix of trials for all cells with drug selection and
     % current selection
@@ -10,35 +26,57 @@ function rslt = drug_svm( mfs, drug_selection, current_selection )
     assignin( 'base', 'svm_mat', svm_mat);
     assignin( 'base', 'drug_labels', drug_labels);
 
-    % Separate into training sets and test set
-    [train_idxs, test_idxs] = separate_trials(size(svm_mat, 1), 0.8);
     
-    train_svm_mat = svm_mat( train_idxs, : ); train_drug_labels = drug_labels( train_idxs );
-    test_svm_mat  = svm_mat( test_idxs, : );  test_drug_labels = drug_labels(  test_idxs );
-    
-    
-    
-    % Train and crossvalidate
-    disp( 'Training SVM...' );
-    
-    drug_label_string = strcat( drug_selection, {'_'}, num2str(current_selection) );
-    SVMModel = fitcsvm(train_svm_mat, train_drug_labels, 'KernelFunction','rbf', 'Standardize',true,'ClassNames',{'Control',drug_label_string{1}});SVMModel = fitcsvm(train_svm_mat, train_drug_labels, 'KernelFunction','rbf', 'Standardize',true,'ClassNames',{'Control','SCH23390_50'});
+    % Alternative Jackknife method of bootstrapping - no replacement. Less
+    % robust.
+    % SVM_Model = fitcsvm( svm_mat, drug_labels );
+    % CVSVMModel = crossval(SVMModel);
+
+    CVSVMModel = struct;
+     for i = 1:straps
+         
+        % Separate into training sets and test set
+        [train_idxs, test_idxs] = separate_trials(size(svm_mat, 1), 0.9); % Train on 90%, leave 10% out
+        train_svm_mat = svm_mat( train_idxs, : ); train_drug_labels = drug_labels( train_idxs );
+        test_svm_mat  = svm_mat( test_idxs, : );  test_drug_labels = drug_labels(  test_idxs );
+
+        % Training SVM
+        disp( strcat( 'Training SVM:', {' '}, num2str(i), '...' ) );
+        SVMModel = fitcsvm(train_svm_mat, train_drug_labels, 'KernelFunction','rbf', 'Standardize',true,'ClassNames',{'Control','Drug'});
+
+        % Test SVM
+        [label,score] = predict( SVMModel, test_svm_mat );
+        perc_corr = compare_labels( label, test_drug_labels );
+        
+        
+        CVSVMModel(i).perc_control = sum(strcmp(train_drug_labels, 'Control')) / length(train_drug_labels);
+        CVSVMModel(i).Model = SVMModel;
+        CVSVMModel(i).label = label; 
+        CVSVMModel(i).score = score;
+        CVSVMModel(i).perc_corr = perc_corr;
+     end
+
+        
+    rslt = CVSVMModel;
+        
     %SVMModel = fitclinear(train_svm_mat, train_drug_labels, 'ClassNames',{'Control', drug_label_string{1} });
-    
-    disp( 'Crossvalidating SVM...' );
-    CVSVMModel = crossval(SVMModel);
-    classLoss = kfoldLoss(CVSVMModel) % The output of this is the generalization rate.
- 
-    
-    % Test
-    [label,score] = predict( SVMModel, test_svm_mat );
-    ScoreSVMModel = fitPosterior( SVMModel, train_svm_mat, train_drug_labels)
-    ScoreTransform = ScoreSVMModel.ScoreTransform
-    
-    
-    
+    %classLoss = kfoldLoss(CVSVMModel) % The output of this is the generalization rate.
+    %ScoreSVMModel = fitPosterior( SVMModel, train_svm_mat, train_drug_labels)
+      
 end
 
+% compare_labels determines how many of the svm labels in a test set were
+% correctly assigned
+function perc_corr = compare_labels( svm_labels, orig_labels )
+    correct = strcmp( svm_labels, orig_labels );
+    perc_corr = sum(correct) / length(svm_labels);
+end
+
+% separate_trials returns two sets of indexes to use to break a matrix of
+% length mat_length into two new matrices. The first set of indices,
+% train_idxs, will contain a fraction of the total indices equal to
+% train_prop. The second set of indices, test_idxs, will contain the
+% remainer.
 function [train_idxs, test_idxs] = separate_trials( mat_length, train_prop )
     idx_vec = 1:mat_length;
     perm_idxs = randperm(numel( idx_vec ));
@@ -51,6 +89,11 @@ function [train_idxs, test_idxs] = separate_trials( mat_length, train_prop )
 end
 
 
+% Iterates through the masterfilestruct and identifies processed files that
+% contain data collected with the chosen drug and current. Returns a large
+% matrix of all the trials appended together. Rows are trials. Columns are
+% time points. The data currently being appended are the spike values (1 or
+% 0) for the 300ms attend window.
 function [svm_mat, drug_labels] = multicell_mat( mfs, drug_selection, current_selection )
 
     svm_mat = [];
@@ -81,7 +124,7 @@ function [svm_mat, drug_labels] = multicell_mat( mfs, drug_selection, current_se
     end
 end
 
-
+% Loads the chosen processed file
 function data_struct = load_processed_file( sub_direc, processed_fname )
 
     if strcmp( comp_mac_address, 'iMac'), save_direc = '/Users/Adrienne/Documents/MATLAB/Cell_Analysis/Processed';
@@ -92,8 +135,9 @@ function data_struct = load_processed_file( sub_direc, processed_fname )
     load( ffp, 'data_struct' );
 end
 
+% Determines which computer the code is being run on - the iMac or the
+% MacBook
 function computer = comp_mac_address()
-
     localhost = java.net.InetAddress.getLocalHost;
     networkinterface = java.net.NetworkInterface.getByInetAddress(localhost);
     macaddress = typecast(networkinterface.getHardwareAddress, 'uint8');
@@ -102,7 +146,6 @@ function computer = comp_mac_address()
         computer = 'iMac';
     else, computer = 'MacBook';
     end
-
 end
 
 
