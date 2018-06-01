d4a4d6aafe92ded8d1094fa707c8197faea8fa8a
adriennemueller <adrienne.l.mueller@gmail.com>
2018-05-31 18:27:54 -0700

    Drug_svm now also generates shuffled data SVMs. Drug_svm_jackknife is an alternative svm that partitions the data for crossvalidation using the jackknife approach.

--- drug_svm_jackknife.m
+++ drug_svm_jackknife.m
@@ -1,23 +1,22 @@
-% drug_svm takes:
+% drug_svm_jackknife takes:
 % master_file_struct
 % drug_selection (either SCH23390 or SKF81297, currently)
 % current: for that drug selection (usually either 20 or 50)
-% straps: the number of crossvalidation iterations to run
+% cross_val_n: the number of data partitions for cross-validation; default 10
 
 % It will create one large matrix of trials from cells which were exposed
 % to the chosen drug at the chosen current and give each trial a label of
-% either 'Control' or 'Drug'. It will then train 'straps'-many SVM
-% classifiers on that data, with the matrix being broken into 'straps'-many
-% sets of training and test matrices.
+% either 'Control' or 'Drug'. It will then partition the matrix into cross_val_n
+% partitions. Cross_val_n - 1 partitions will be used as a trianing set and
+% the remainder as the test set. Cross_val_n SVMs will be trained. The
+% comparison of original labels and predicted labels (length of original
+% matrix of trials) will be calculated and included in the output - as well
+% as all of the trained models. 
 
-% The output struct includes the models, the labels, the scores and the percent of
-% the labels that were correct for each model. The struct will also
-% include, for each model, the percentage of the training matrix trials
-% that were control trials.
-function rslt = drug_svm( mfs, drug_selection, current_selection, straps, scramble )
+% The output struct includes the models, the labels, the scores.
+function rslt = drug_svm_jackknife( mfs, drug_selection, current_selection, crossval_n )
     
-    if nargin < 5, scramble = 0; end
-    if nargin < 4, straps = 100; end
+    if nargin < 4, crossval_n = 10; end
 
     % Get giant matrix of trials for all cells with drug selection and
     % current selection
@@ -26,57 +25,77 @@ function rslt = drug_svm( mfs, drug_selection, current_selection, straps, scramb
     
     assignin( 'base', 'svm_mat', svm_mat);
     assignin( 'base', 'drug_labels', drug_labels);
-
     
-    % Alternative Jackknife method of bootstrapping - no replacement. Less
-    % robust.
-    % SVM_Model = fitcsvm( svm_mat, drug_labels );
-    % CVSVMModel = crossval(SVMModel);
-
-    CVSVMModel = struct;
-     for i = 1:straps
-         
-        % Separate into training sets and test set
-        [train_idxs, test_idxs] = separate_trials(size(svm_mat, 1), 0.9); % Train on 90%, leave 10% out
-        train_svm_mat = svm_mat( train_idxs, : ); train_drug_labels = drug_labels( train_idxs );
-        test_svm_mat  = svm_mat( test_idxs, : );  test_drug_labels = drug_labels(  test_idxs );
-
-        % Training SVM
+    % Separate data into crossvalidation partitions 
+    partition_struct = partition_svm_mat( svm_mat, drug_labels, cross_val_n );
+    
+    % Generating training and test sets from the 10 partions
+    svm_struct = gen_crossval_svm_struct( partition_struct );
+    
+    % Loop through svm_struct, generating an SVM for each train/test set
+    % and predicting test values. Add predicted test labels to list
+    % compared with real labels
+    for i = 1:length(svm_struct)
+        % Train SVM
         disp( strcat( 'Training SVM:', {' '}, num2str(i), '...' ) );
+        train_svm_mat = svm_struct(i).train_svm_mat;
+        train_drug_labels = svm_struct(i).train_drug_labels;
+        test_svm_mat = svm_struct(i).test_svm_mat;
+        test_drug_labels = svm_struct(i).test_drug_labels;
         SVMModel = fitcsvm(train_svm_mat, train_drug_labels, 'KernelFunction','rbf', 'Standardize',true,'ClassNames',{'Control','Drug'});
         
         % Test SVM
         [label,score] = predict( SVMModel, test_svm_mat );
         perc_corr = compare_labels( label, test_drug_labels );
-        
-        CVSVMModel(i).perc_control = sum(strcmp(train_drug_labels, 'Control')) / length(train_drug_labels);
-        CVSVMModel(i).Model = SVMModel;
-        CVSVMModel(i).label = label; 
-        CVSVMModel(i).score = score;
-        CVSVMModel(i).perc_corr = perc_corr;
-        
-        if scramble
-            scramble_train_drug_labels = train_drug_labels(randperm(length(train_drug_labels)));
-            disp( strcat( 'Training Scrambled SVM:', {' '}, num2str(i), '...' ) );
-            Scramble_SVMModel = fitcsvm(train_svm_mat, scramble_train_drug_labels, 'KernelFunction','rbf', 'Standardize',true,'ClassNames',{'Control','Drug'});
-            [scr_label, scr_score] = predict( Scramble_SVMModel, test_svm_mat );
-            scr_perc_corr = compare_labels( scr_label, test_drug_labels );
 
-            CVSVMModel(i).Scamble_Model = Scramble_SVMModel;
-            CVSVMModel(i).scr_label = scr_label; 
-            CVSVMModel(i).scr_score = scr_score;
-            CVSVMModel(i).scr_perc_corr = scr_perc_corr;
-        end
-     end
-
+        svm_struct(i).predicted_labels = label;
+        svm_struct(i).score = score;
+        svm_struct(i).Model = SVMModel;
         
-    rslt = CVSVMModel;
-        
-    %SVMModel = fitclinear(train_svm_mat, train_drug_labels, 'ClassNames',{'Control', drug_label_string{1} });
-    %classLoss = kfoldLoss(CVSVMModel) % The output of this is the generalization rate.
-    %ScoreSVMModel = fitPosterior( SVMModel, train_svm_mat, train_drug_labels)
+    end
+    
+    % Perform sign test on 'left out' trials
+    rslt
       
 end
+
+% Separate a struct containing n trial matrices into n training sets and
+% test sets
+function svm_struct = gen_crossval_svm_struct( partition_struct )
+
+    svm_struct = struct;
+
+    for left_out_idx = 1:length(partition_struct)
+       
+        svm_struct(left_out_idx).train_svm_mat = vertcat(partition_struct( ~ left_out_idx ).svm_mat);
+        svm_struct(left_out_idx).train_drug_labels = vertcat(partition_struct( ~ left_out_idx ).orig_labels);
+        
+        svm_struct(left_out_idx).test_svm_mat = vertcat(partition_struct( left_out_idx ).svm_mat);
+        svm_struct(left_out_idx).test_drug_labels = vertcat(partition_struct( left_out_idx ).orig_labels);
+
+    end
+
+end
+
+
+% Break a matrix of trials and a matrix of data labels into cross_val_n
+% number of partitions. Return them in a struct.
+function partition_struct = partition_svm_mat( svm_mat, drug_labels, cross_val_n )
+
+    partition_struct = struct;
+
+    num_trials = size(svm_mat,1);
+    partition_idxs = crossvalind('Kfold', num_trials, crossval_n);
+
+    for i = 1:max(partition_idxs)
+
+        partition_struct(i).svm_mat = svm_mat(partition_idxs == i,:);
+        partition_struct(i).orig_labels = drug_labels(partition_idxs == i,:);
+        
+    end
+
+end
+
 
 % compare_labels determines how many of the svm labels in a test set were
 % correctly assigned
