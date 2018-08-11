4dd1c475d16c9ed504e42c23db7030bbfef621b5
adriennemueller <adrienne.l.mueller@gmail.com>
2018-08-11 15:38:41 -0700

    More sden for gen_overview_fig.

--- filtered_windowed_spikemat.m
+++ filtered_windowed_spikemat.m
@@ -17,7 +17,7 @@ function rslt_mat = filtered_windowed_spikemat( curr_data_mat, current, window, 
     valid_idxs = correct_idxs( ismember( correct_idxs, corr_current_idxs ) );
 
     % Filter for (attend) direction
-    if strcmp( inout, 'out' )
+    if ~ isempty( inout) && strcmp( inout, 'out' )
         direction = reversed(direction);
     end
         
@@ -25,36 +25,39 @@ function rslt_mat = filtered_windowed_spikemat( curr_data_mat, current, window, 
         corr_direc_idxs = find( [curr_data_mat.theta] == direction );
         valid_idxs = valid_idxs( ismember( valid_idxs, corr_direc_idxs ) );
     end    
-    
-    % Filter for window
+   
+     % Filter for window
     spike_mat = {curr_data_mat(valid_idxs).spikes};
     millis_mat = {curr_data_mat(valid_idxs).millis};
-    rslt_mat = extract_window( window, spike_mat, millis_mat, {curr_data_mat(valid_idxs).event_codes}, {curr_data_mat(valid_idxs).code_times} );
+    event_codes = {curr_data_mat(valid_idxs).event_codes};
+    code_times = {curr_data_mat(valid_idxs).code_times};
+    rslt_mat = extract_window( window, spike_mat, millis_mat, event_codes, code_times  );
     
 end
 
 
 % Make a smaller spike matrix of just the specified window
-function windowed_mat = extract_window( window, spike_mat, millis_mat, event_codes, code_times )
-    win_beg = window(1);
-    win_end = window(2);
+function windowed_mat = extract_window( window, spike_mat, millis_mat, event_codes, code_times)
     
+    win_end      = window(1);
+    win_length   = window(2);
+
     % Loops over matching cells in event_codes and code_times and applies
-    % the find of the win_beg and win_end event_codes to each pair.
-    win_beg_times = cellfun(@(codes, times) times(codes == win_beg), event_codes, code_times);
+    % the find of the win_end event_codes to each pair.
     win_end_times = cellfun(@(codes, times) times(codes == win_end), event_codes, code_times);
-        
-    % Find the indices of the milli value that corresponds to those times.
-    win_beg_idxs = cellfun(@(millis, times) find(millis == times), millis_mat, num2cell(win_beg_times), 'uniformoutput', 0);
-    win_end_idxs = cellfun(@(millis, times) find(millis == times), millis_mat, num2cell(win_end_times), 'uniformoutput', 0);
+
     
+    % Find the indices of the milli value that corresponds to those times.
+    win_end_idxs = cellfun(@(millis, times) find(millis == times), millis_mat, num2cell(win_end_times), 'uniformoutput', 0);
+
     % Grab the chunk of spikes from beginning index to end index
-    windowed_cellarray = cellfun(@(spikes, beg_idxs, end_idxs) spikes( beg_idxs:end_idxs ), spike_mat, win_beg_idxs, win_end_idxs, 'uniformoutput', 0);
+    %windowed_cellarray = cellfun(@(spikes, beg_idxs, end_idxs) spikes( beg_idxs:end_idxs ), spike_mat, win_beg_idxs, win_end_idxs, 'uniformoutput', 0);
+    windowed_cellarray = cellfun(@(spikes, end_idxs) spikes( end_idxs - win_length:end_idxs ), spike_mat, win_end_idxs, 'uniformoutput', 0);
     
     % Shave off any excess time_bins so can make a matrix. Shave off from
     % the front. This feels a little dodgy, but I think it's okay as long
     % as acknowledge in methods.
-    windowed_cellarray = truncate( windowed_cellarray );
+    %windowed_cellarray = truncate( windowed_cellarray );
     
     % Convert to matrix
     windowed_mat = cell2mat(windowed_cellarray);
