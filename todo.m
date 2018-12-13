5dde8ea5cab4b278666e4dc43958d42ca077fff1
adriennemueller <adrienne.l.mueller@gmail.com>
2018-12-13 15:23:46 -0800

    Removed files that are no longer used and updated todo list.

--- todo.m
+++ todo.m
@@ -1,33 +1,34 @@
+
+% Handle gen_overview_figs for WM trials
+% Figure out what's up with probe trials - how to handle.
+%     MOTOR WINDOW
+
+
 %%% Want function to make summary anova fig - how many cells with attend
 %%% effects exhibit drug/attend interactions. How many drug effects etc.
+% Merge gen_anova_counts and gen_anova_contrast_counts
 
 
 % Should combined drug_svm? (jackknife also)
 % MAKE SVM MORE GENERIC FOR other label types (attin/attout, e.g.)
 % Make attend_svm not a thing (a hacked copy-paste of drug_svm )
+% Make a better version of make_drug_plots for output of SVM analysis.
 
-% Handle gen_overview_figs for WM trials
 
 % Print out thetas, trials counts, durations etc to make sure looks okay?
 
-% Figure out what's up with probe trials - how to handle.
+
+% Make a better version of     plot_contrast_dprimes; which doesn't require
+% manual rotation / does something better than the rotation.
+
 
 %%% THINK %%%
 
-% Plot SVM performanc as increase number of neurons included
+% Plot SVM performance as increase number of neurons included
 
 % Plot SVM performance vs scrambled SVM Performance (unity plot? or two
 % histograms)
 
-
-Remove?
-    evaluate_units?
-    segregate_stat_struct?
-    spike_density spike_density_psp and spike raster?
-   
-    Check others - update-spikemat? shadedErrorBar? add_session?
-    
-    MOTOR WINDOW
     
 Make ReadMe with Workflow?
  make_master_file_struct
