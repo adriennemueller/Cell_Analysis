57fb35fcaddb976dc36c696c69787f95df0d5f9b
adriennemueller <adrienne.l.mueller@gmail.com>
2019-10-08 17:07:08 -0700

    Preparing for comparison fig.

---
+++ compare_drug_across_phases.m
@@ -0,0 +1,23 @@
+function rslt = compare_drug_across_phases( mfs )
+
+    % Attend Trials / Different Drug Currents
+    
+    
+    % WM Trials / Different Drug Currents
+
+
+end
+
+function 
+
+
+    % For all neurons
+
+    % Get average responses (mean firing rates) for each phase of the trial
+    
+    % For drug on and drug off
+    
+    % Perform anova with target position as a factor 
+
+    % Make a unity plot for this epoch and drug
+
--- todo.m
+++ todo.m
@@ -1,11 +1,4 @@
 
-% Handle gen_overview_figs for WM trials
-% Figure out what's up with probe trials - how to handle.
-%     MOTOR WINDOW
-% Show full trial length
-% Label epochs appropriately. 
-% Make sure epochs correct
-
 %%% Want function to make summary anova fig - how many cells with attend
 %%% effects exhibit drug/attend interactions. How many drug effects etc.
 % Merge gen_anova_counts and gen_anova_contrast_counts
@@ -16,18 +9,12 @@
 % Make attend_svm not a thing (a hacked copy-paste of drug_svm )
 % Make a better version of make_drug_plots for output of SVM analysis.
 
-
-% Print out thetas, trials counts, durations etc to make sure looks okay?
-
-
-% Make a better version of     plot_contrast_dprimes; which doesn't require
+% Make a better version of plot_contrast_dprimes; which doesn't require
 % manual rotation / does something better than the rotation.
 
-% USE MI instead of d' (as option)
+% Perform SVM on drug-no drug for different epochs
 
-% Go through and toss out bad/duplicate units so working with actual clean
-% dataset.
-
+% Consider unrewarded trials?
 
 %%% THINK %%%
 
