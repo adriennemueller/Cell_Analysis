d428e2a04b2a0ea7f3c358c791101f72b097ff1c
Adrienne Mueller <adrienne.l.mueller@gmail.com>
2019-11-08 00:15:16 -0800

    todolist update

--- todo.m
+++ todo.m
@@ -8,6 +8,9 @@ MAKE SVM MORE GENERIC FOR other label types (attin/attout, e.g.)
 Make attend_svm not a thing (a hacked copy-paste of drug_svm )
 Make a better version of make_drug_plots for output of SVM analysis.
 
+Plot SVM performance as increase number of neurons included
+Plot SVM performance vs scrambled SVM Performance (unity plot? or two histograms)
+
 Make a better version of plot_contrast_dprimes; which doesn't require
     manual rotation / does something better than the rotation.
 
@@ -15,21 +18,18 @@ Perform SVM on drug-no drug for different epochs
 
 Consider unrewarded trials?
 
-Plot SVM performance as increase number of neurons included
-Plot SVM performance vs scrambled SVM Performance (unity plot? or two histograms)
 
 Make anova also handle attend direction as a factor
 Verify d' does what it's supposed to for WM
-Make summary figures look prettier: extend x axis, add cue and current and paradigm labels
 Make summaryfig work in combining att_fig and wm_fig
 Make overviewfig show 8dirs for wm
-Check alignment on early files; look a little weird.
+Check alignment on early files; looks a little weird.
 Population comparison that allows for theta variability?
     
 Make ReadMe with Workflow?
  make_master_file_struct
  preprocess
  process_stats
- - gen_overview_figs
- - gen_unity_plots
+ gen_overview_figs
+ gen_unity_plots
  
